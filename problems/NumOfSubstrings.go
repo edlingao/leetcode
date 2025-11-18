660f7457adaeb14c4cabfcfadf9470fb41d5414b
edlingao <sonido244@gmail.com>
2025-11-18 16:17:38 -0600

    Reflecting the real problem I uploaded

--- problems/NumOfSubstrings.go
+++ problems/NumOfSubstrings.go
@@ -5,7 +5,6 @@ import (
 )
 
 var MOD float64 = 1e9 + 7
-var DICT map[string]int = map[string]int{}
 
 func NumSub(s string) int {
 	arr := strings.Split(s, "0")
@@ -17,10 +16,6 @@ func NumSub(s string) int {
 }
 
 func sumTotals(s string) int {
-	if DICT[s] != 0 {
-		return DICT[s]
-	}
-
 	n := strings.Count(s, "1")
 	sum := 0
 	for i := 1; i <= n; i++ {
